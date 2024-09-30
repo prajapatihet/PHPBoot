a450ac81fc6147417ec099040a0d6dafb7a87bf5
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-09-30 22:11:11 +0530

    escape_seq

---
+++ escape_seq.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP-EscapeSequence</title>
+</head>
+<body> 
+    <h1>Hello World</h1>
+    <?php 
+        echo "<h1>Escape \" or \\ is used</h1>";
+        echo "<pre>How are \t you? </pre>"
+    ?>
+</body>
+</html>
