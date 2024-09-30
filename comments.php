8a87f091b9ea50c6bea740fd9fcfea0338457c58
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-09-30 11:22:29 +0530

    comments

---
+++ comments.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP-Comments</title>
+</head>
+<body> 
+    <h1>Hello World</h1>
+    <?php 
+        // This is single line comment
+        # This is also single comment
+        /*
+            This is multi 
+            line comment
+        */
+    ?>
+</body>
+</html>
