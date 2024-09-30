caa4b7955db212d9740410d9f23ebd30d982b406
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-09-30 11:22:47 +0530

    keywords

---
+++ php_keywords.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>PHP-Reserved Keyword</title>
+</head>
+<body>
+    <h1>Reserved Keywords in PHP</h1>
+    <img src="./keywords.png" alt="keywords_img">
+</body>
+</html>
