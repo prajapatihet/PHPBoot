ec118435d5a59dea705f1939462fb5fd4236c666
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-09-30 10:54:22 +0530

    hello_world

---
+++ hello_world.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP Hello World</title>
+</head>
+<body> 
+    <h1>Hello World</h1>
+    <?php 
+        echo "<h1>Using Echo PHP</h1>";
+    ?>
+</body>
+</html>
