d9247f60c6e6f2791037d37db61cba7918ae15d4
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-10-04 20:14:24 +0530

    arithmetic

---
+++ arithmetic_op.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Arithmetic Operators</title>
+</head>
+<body>
+    <!-- demostration of all arithmetic operation -->
+     <?php 
+        echo "Arithmetic Operators";
+        echo "<br>";
+        //declaring variables
+        $a = 10;
+        $b = 5;
+        echo "Addition = ".$a + $b;
+        echo "<br>";
+        echo "Subtration = ".$a - $b;
+        echo "<br>";
+        echo "Multiplication = ".$a * $b;
+        echo "<br>";
+        echo "Division = ".$a / $b;
+        echo "<br>";
+        echo "Modulo = ".$a % $b;
+
+     ?>
+</body>
+</html>
